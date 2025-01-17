561f3c9cec332882df6593d598a97bd4b0206884
Rakibulislam-emon <rakibulislamemon60>
2025-01-17 13:57:04 +0600

    route fixed

--- src/pages/Cart/OrderSummary.jsx
+++ src/pages/Cart/OrderSummary.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router";
+import useAuth from "../../hooks/useAuth";
 
 const OrderSummary = ({ subtotal, shipping, tax, total }) => {
+  const {user} = useAuth()
+
+
     return (
       <div className="shadow-md p-6 lg:sticky lg:top-0 h-max">
         <h3 className="text-lg font-bold text-gray-800 border-b pb-4">Order Summary</h3>
   
-        <ul className="text-gray-800 divide-y mt-4">
+        <ul className="text-gray-800 divide</ul>-y mt-4">
           <li className="flex flex-wrap gap-4 text-sm py-3">
             Subtotal <span className="ml-auto font-bold">${subtotal}</span>
           </li>
@@ -21,7 +25,7 @@ const OrderSummary = ({ subtotal, shipping, tax, total }) => {
           </li>
         </ul>
   
-        <Link to={'/checkout'}>
+        <Link to={user ? "/checkout" : "/authentications"}>
           <button
             type="button"
             className="mt-4 text-sm px-5 py-2.5 w-full bg-blue-600 hover:bg-blue-700 text-white rounded-md"
